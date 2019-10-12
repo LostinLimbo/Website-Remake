b42016af136fdc653b02d7bc34e023dbb22b2d6e
Ben Mortimer <50639398+LostinLimbo@users.noreply.github.com>
2019-10-12 11:48:19 +0100

    updates

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,10 +1,10 @@
 
-// Make the navbar disappear on scroll down and reappear on scroll up.
+let list = document.querySelector(".cycle > li");
 let nav = document.querySelector("nav");
 let new_scroll_position = 0;
 let last_scroll_position;
 
-
+// Make the navbar disappear on scroll down and reappear on scroll up.
 window.addEventListener('scroll', function(e) {
   last_scroll_position = window.scrollY;
 
@@ -21,3 +21,21 @@ window.addEventListener('scroll', function(e) {
 
   new_scroll_position = last_scroll_position;
 });
+
+// Cycle through the services offered!
+var services = [
+  "Fed Up with Books and Paperwork?",
+  "<img src=\"Assets\/CSS\/Img-lib\/pages-image.gif\">",
+  "We Can Help!",
+  "Free initial Consultation",
+  "Individual Self-Assessment Tax Returns",
+  "P.A.Y.E. Scheme, Set up and Control",
+  "Business Start-ups",
+  "Sole Traders",
+  "Partnerships",
+  "Privately Owned Small Limited Companies"
+];
+
+
+
+
